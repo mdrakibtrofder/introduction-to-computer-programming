6f3d865a6a7c820055f79855b650f0337302a10c
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-02-22 13:07:42 +0600

    add point and salary problem solution

---
+++ week_2_lab/point_distance_calculation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x1, y1, x2, y2, distance;
+
+    printf("Enter x1");
+    scanf("%lf", &x1);
+
+    printf("Enter y1");
+    scanf("%lf", &y1);
+
+    printf("Enter x2");
+    scanf("%lf", &x2);
+
+    printf("Enter y2");
+    scanf("%lf", &y2);
+
+    distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+    printf("Distance = %.2f\n", distance);
+
+    return 0;
+}
